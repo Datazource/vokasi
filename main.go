751ebbf69d2841930804515f9b910cf1d9e49e0f
Paulus Donny Junianto <paulus.junianto@bytedance.com>
2024-09-03 08:55:22 +0700

    Update umur

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import "fmt"
 
 func main() {
 	var name string
+	var age int
 
 	fmt.Print("Masukkan Nama Anda: ")
 	fmt.Scan(&name)
+	fmt.Print("Masukkan Umur Anda: ")
+	fmt.Scan(&age)
 	fmt.Printf("Hello %s !\n", name)
+	fmt.Printf("Umur anda %s !\n", age)
 }
